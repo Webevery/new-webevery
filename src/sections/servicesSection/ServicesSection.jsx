30c9d35cc290d49fe60af3eeff66b12d20b420f2
Maksym Padalka <sheihmaks@gmail.com>
2024-02-07 12:24:08 +0100

    Update ServicesSection.jsx

--- src/sections/servicesSection/ServicesSection.jsx
+++ src/sections/servicesSection/ServicesSection.jsx
@@ -1,7 +1,19 @@
 import styles from "./ServicesSection.module.scss";
 
 const ServicesSection = () => {
-  return <section>ServicesSection</section>;
+  return <section>
+    <div>
+      <h2>Послуги <span>від малого до великого сайту</span></h2>
+      <ul>
+        <li>
+          <h3>Сайт-візитка</h3>
+        </li>
+        <li>
+          <h3>Лендінг</h3>
+        </li>
+      </ul>
+    </div>
+  </section>;
 };
 
 export default ServicesSection;
