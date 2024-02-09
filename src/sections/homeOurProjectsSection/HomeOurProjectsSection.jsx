5610a74f10813bd26c568f34ecf5d6b09a36c14b
Maksimchik36 <Peltek1985@gmail.com>
2024-02-09 17:09:46 +0200

    added global titleGradient class, changed some styles, completed HomeOurProjectsSection without slider,

--- src/sections/homeOurProjectsSection/HomeOurProjectsSection.jsx
+++ src/sections/homeOurProjectsSection/HomeOurProjectsSection.jsx
@@ -1,7 +1,18 @@
+import NavigationBtn from "@/components/Buttons/NavigationBtn/NavigationBtn";
 import styles from "./HomeOurProjectsSection.module.scss";
 
+
 const HomeOurProjectsSection = () => {
-  return <section>HomeOurProjectsSection</section>;
+  return <section className={styles.container}>
+    <div className="container">
+      <h1 className={styles.title}>Our <span className="titleGradient">projects</span>
+      </h1>
+      <div className={styles.contentWrapper}>
+        <div className={styles.slider}>Slider</div>
+        <NavigationBtn title="To portfolio" href={"/ourProjects"} className={styles.toPortfolioBtn} />
+      </div>
+    </div>
+  </section>;
 };
 
 export default HomeOurProjectsSection;
