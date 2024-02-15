dff252ae68bedbdefdf8a96c09782d47e170fc9b
Maksym Padalka <sheihmaks@gmail.com>
2024-02-15 10:18:47 +0100

    Update Services.jsx

--- src/screens/services/Services.jsx
+++ src/screens/services/Services.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Services.module.scss";
 import ServicesSection from "@/sections/servicesSection/ServicesSection";
 
-
 const Services = () => {
   return <div className="screen">
     <ServicesSection />
