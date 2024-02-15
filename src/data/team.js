f5dd2a006a1b9c71b68c5a1ffd1169ab26e05afe
Denis <denis.tolmachev@gmail.com>
2024-02-15 12:14:25 +0200

    page structure creation

---
+++ src/data/team.js
@@ -0,0 +1,88 @@
+import { v4 } from "uuid";
+
+export const team = [
+  {
+    id: v4(),
+    img: "",
+    name: "Volodymyr Kavun",
+    jobTitle:
+      "Team-Lead, Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Nataliia Boiko",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Maksim Peltek",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Maksym Padalka",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Ivika Duda",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Dmytro Kolodchenko",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Andrii Tsapkov",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Denys Tolmachov",
+    jobTitle:
+      "Full Stack Developer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Denys Kudriavets",
+    jobTitle:
+      "QA Engineer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Dmitro Kotenov",
+    jobTitle:
+      "UX/UI/Designer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Viktoriia Svyrydova",
+    jobTitle:
+      "UX/UI/Designer",
+  },
+  {
+    id: v4(),
+    img: "",
+    name: "Anastasiia Kuznietsova",
+    jobTitle:
+      "UX/UI/Designer",
+  },
+];
--- src/sections/teamSection/TeamSection.jsx
+++ src/sections/teamSection/TeamSection.jsx
@@ -1,7 +1,42 @@
 import styles from "./TeamSection.module.scss";
+import { team } from '@/data/team';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const TeamSection = () => {
-  return <section>TeamSection</section>;
+  return (
+    <section>
+      <section className={styles.team}>
+        <div className={`container ${styles.teamContainer}`}>
+          <div className={styles.titleTeamContainer}>
+            <h1 className={styles.titleTeam}>
+              <span>Team</span>
+            </h1>
+            <h2 className={styles.descTeam}>
+            let&apos;s get to know those who work for you
+            </h2>
+          </div>
+          <ul className={styles.cartContainer}>
+            {team.map(({ id, img, name, jobTitle }) => (
+              <li key={id} className={styles.cartItem}>
+                <div className={styles.cartImgContainer}>
+                  <Image
+                    src={img}
+                    alt="img team"
+                    fill="true"
+                    className={styles.cartImg}
+                  />
+                </div>
+
+                <h3 className={styles.cartName}>{name}</h3>
+                <p className={styles.cartJobTitle}>{jobTitle}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default TeamSection;
