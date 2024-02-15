a823c9d399c67ff8b1e11da95065450c2e8af4ec
Nataliia <natalabojko@gmail.com>
2024-02-15 14:25:30 +0200

    headerSubLinksInProcess

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -2,26 +2,72 @@
 
 import { SiteContext } from "@/context/siteContext";
 import Link from "next/link";
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { navLinks } from "../../data/navLinks";
 import styles from "./NavLinks.module.scss";
 
 const NavLinks = ({ className }) => {
   const { burgerMenu, setBurgermenu } = useContext(SiteContext);
 
-  const isDocument = typeof window !== "undefined";
+  const isClient = typeof window !== "undefined";
+
+  const refSubLinkWrapper = useRef(null);
+  const refArrow = useRef(null);
+
+  const subLinkWrapper = refSubLinkWrapper.current;
+  const arrow = refArrow.current;
+  console.log("arrow", arrow);
+
+  const toggleSubLinkWrapper = () => {
+    if (subLinkWrapper.classList.contains(`${styles.subLinkWrapperVisible}`)) {
+      subLinkWrapper.classList.remove(`${styles.subLinkWrapperVisible}`);
+      subLiarrownkWrapper.classList.add(`${styles.arrowActive}`);
+    } else {
+      subLinkWrapper.classList.add(`${styles.subLinkWrapperVisible}`);
+      subLiarrownkWrapper.classList.remove(`${styles.arrowActive}`);
+    }
+  };
 
   useEffect(() => {
-    if (burgerMenu && isDocument) {
+    if (burgerMenu && isClient) {
       document.body.style.overflowY = "hidden";
-    } else if (isDocument) {
+    } else if (isClient) {
       document.body.style.overflowY = "scroll";
     }
-  }, [burgerMenu, isDocument]);
+  }, [burgerMenu, isClient]);
 
   const links = navLinks.map((link) => {
     return (
       <div key={link.id} className={styles.linkWrapper}>
+        {link.subMenu && (
+          <svg
+            className={styles.arrow}
+            onClick={toggleSubLinkWrapper}
+            ref={refArrow}
+          >
+            <use href="/sprite.svg#icon-NavArrow"></use>
+          </svg>
+        )}
+        {link.subMenu && (
+          <div className={styles.subLinkWrapper} ref={refSubLinkWrapper}>
+            {link.subMenu?.map((sub) => {
+              return (
+                <Link
+                  key={sub.id}
+                  href={`${link.href}/${sub.href}`}
+                  onClick={() => {
+                    setBurgermenu(false);
+                    subLinkWrapper.classList.remove(
+                      `${styles.subLinkWrapperVisible}`
+                    );
+                  }}
+                >
+                  {sub.title}
+                </Link>
+              );
+            })}
+          </div>
+        )}
         <Link
           href={link.href}
           className={styles.navLink}
@@ -31,23 +77,6 @@ const NavLinks = ({ className }) => {
         >
           {link.title}
         </Link>
-        {link.subMenu && (
-          <div className={styles.subLinkWrapper}>
-            {link.subMenu?.map((sub) => {
-              return (
-                <Link
-                  key={sub.id}
-                  href={`${link.href}/${sub.href}`}
-                  onClick={() => {
-                    setBurgermenu(false);
-                  }}
-                >
-                  {sub.title}
-                </Link>
-              );
-            })}
-          </div>
-        )}
       </div>
     );
   });
