131306b2400c51aae0410423884f6734f3e527dd
Maksym Padalka <sheihmaks@gmail.com>
2024-02-25 00:51:49 +0100

    +

--- src/sections/homeServicesSection/HomeServicesSection.jsx
+++ src/sections/homeServicesSection/HomeServicesSection.jsx
@@ -1,3 +1,4 @@
+import NavigationBtn from "@/components/Buttons/NavigationBtn/NavigationBtn";
 import styles from "./HomeServicesSection.module.scss";
 
 import { SliderOfServices } from "@/components/SliderOfServices/SliderOfServices";
@@ -6,7 +7,9 @@ const HomeServicesSection = () => {
   return <section className={styles.container}>
     <div className="container">
       <h2 className={styles.title}>Послуги</h2> 
-      <h3 className={styles.subTitleLaptop}>від малого до великого сайту</h3>
+      <h3 className={`titleGradient ${styles.subTitleLaptop}`}>від малого до великого сайту</h3>
+      <NavigationBtn
+      title='Детальніше'/>
       <SliderOfServices/>
     </div>
   </section>;
