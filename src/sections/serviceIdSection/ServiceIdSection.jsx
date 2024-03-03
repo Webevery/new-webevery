bde87aa1e4585b9f326d2aa9c2833380a6249efa
Denis <denis.tolmachev@gmail.com>
2024-03-03 20:58:53 +0200

    server data translation, gradient correction

--- src/sections/serviceIdSection/ServiceIdSection.jsx
+++ src/sections/serviceIdSection/ServiceIdSection.jsx
@@ -6,11 +6,16 @@ import { CldImage } from "next-cloudinary";
 import OrderBtn from "../../components/Buttons/OrderBtn/OrderBtn";
 import { SliderOfServices } from "@/components/SliderOfServices/SliderOfServices";
 
+import { useTranslation } from "react-i18next";
+import { currentLanguages } from "@/data/languages";
+
 const ServiceIdSection = ({ params }) => {
   const { slug } = params;
   const { data, error, isLoading } = GetIdDataFromSection("services", slug);
 
   const dataId = data && !isLoading ? data : error;
+
+  const { i18n } = useTranslation();
 
   console.log(dataId);
 
@@ -25,23 +30,33 @@ const ServiceIdSection = ({ params }) => {
         <div className={styles.servicesIdContent}>
           <div className={styles.servicesIdImgContainer}>
             <CldImage
-            src={dataId?.mockup}
-            alt={dataId?.title}
-            fill="true"
-            className={styles.cartImg}
-            sizes="30vw"
-          />
+              src={dataId?.mockup}
+              alt={dataId?.title}
+              fill="true"
+              className={styles.cartImg}
+              sizes="30vw"
+            />
           </div>
-          
+
           <div className={styles.servicesIdDescContainer}>
-            <p className={styles.servicesIdDesc}>{dataId?.description}</p>
-            <p className={styles.servicesIdPrice}>{dataId?.price}</p>
+            <p className={styles.servicesIdDesc}>
+              {i18n.language === currentLanguages.EN
+                ? dataId?.descriptionEn
+                : dataId?.description}
+            </p>
+            <p className={styles.servicesIdPrice}>
+              {i18n.language === currentLanguages.EN
+                ? dataId?.priceEn
+                : dataId?.price}
+            </p>
             <OrderBtn id={styles.serviceOrderBtn} title={"Замовити"} />
           </div>
         </div>
 
         <div className={`container ${styles.servicesIdSliderContainer}`}>
-          <h3 className={styles.servicesIdSliderTitle}>Оберіть <span>найкращу</span> пропозицію для вашого бізнесу:</h3>
+          <h3 className={styles.servicesIdSliderTitle}>
+            Оберіть <span>найкращу</span> пропозицію для вашого бізнесу:
+          </h3>
           <SliderOfServices />
         </div>
       </div>
