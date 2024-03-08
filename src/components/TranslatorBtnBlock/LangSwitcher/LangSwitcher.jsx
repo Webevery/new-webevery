c66928189a7f55e4026aab805dd0c5ce5ce51566
Nataliia <natalabojko@gmail.com>
2024-03-08 10:19:52 +0200

    ToTopBtn fixed, mobile Menu close onClick fixed

--- src/components/TranslatorBtnBlock/LangSwitcher/LangSwitcher.jsx
+++ src/components/TranslatorBtnBlock/LangSwitcher/LangSwitcher.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./LangSwitcher.module.scss";
 
-const LangSwitcher = ({ changeLanguage, currentLanguage }) => {
+const LangSwitcher = ({ changeLanguage, currentLanguage, ref }) => {
   // const[lang, setLang]=useState(()=>{
   //   currentLanguage || !currentLanguage === "ua" ? "ua" : "en"
   // })
@@ -10,19 +10,29 @@ const LangSwitcher = ({ changeLanguage, currentLanguage }) => {
   //   currentLanguage === "ua" ? setLang ("ua") : setLang("en")
   // },[currentLanguage])
 
-  const onHandleSetUa=()=>{
-    const languageUser= "ua"
-    changeLanguage(languageUser)
-  }
+  const onHandleSetUa = () => {
+    const languageUser = "ua";
+    changeLanguage(languageUser);
+  };
 
-  const onHandleSetEng=()=>{
-    const languageUser= "en";
-    changeLanguage(languageUser)
-  }
+  const onHandleSetEng = () => {
+    const languageUser = "en";
+    changeLanguage(languageUser);
+  };
   return (
-    <ul className={styles.langsWrapper} >
-      <li className= {currentLanguage==="ua" ?  styles.notActive : styles.active} onClick={onHandleSetEng}>ENG</li>
-      <li className= {currentLanguage==="en" ? styles.notActive : styles.active} onClick={onHandleSetUa}>UKR</li>
+    <ul className={styles.langsWrapper}>
+      <li
+        className={currentLanguage === "ua" ? styles.notActive : styles.active}
+        onClick={onHandleSetEng}
+      >
+        ENG
+      </li>
+      <li
+        className={currentLanguage === "en" ? styles.notActive : styles.active}
+        onClick={onHandleSetUa}
+      >
+        UKR
+      </li>
     </ul>
   );
 };
--- src/components/TranslatorBtnBlock/TranslatorBtnBlock.jsx
+++ src/components/TranslatorBtnBlock/TranslatorBtnBlock.jsx
@@ -1,33 +1,44 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import LangSwitcher from "./LangSwitcher/LangSwitcher";
 
-const TranslatorBtnBlock=({className})=>{
-    const {i18n}=useTranslation();
-    const[language, setLanguage]=useState(
-        (prev)=> !prev || prev === undefined ? "ua" : prev
-        );
-    const [isLoad, setIsLoad] = useState(true);
-    
-    useEffect(()=>{
-        const lang= localStorage.getItem("whatLanguage")
-        setLanguage(()=> lang ? lang : "ua")
-        
-        setIsLoad(false)
-    },[]);
+const TranslatorBtnBlock = ({ className, ref }) => {
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState((prev) =>
+    !prev || prev === undefined ? "ua" : prev
+  );
+  const [isLoad, setIsLoad] = useState(true);
 
-    const changeLanguage=(languageUser)=>{
-        localStorage.setItem("whatLanguage",languageUser);
-        const language= localStorage.getItem("whatLanguage")
-        setLanguage(language)
-        i18n.changeLanguage(language)
-    }
-    return (<div className={className}>
-        {!isLoad && <LangSwitcher 
-        changeLanguage={changeLanguage}
-        currentLanguage={language} />}
-        </div>)
-}
+  useEffect(() => {
+    const lang = localStorage.getItem("whatLanguage");
+    setLanguage(() => (lang ? lang : "ua"));
 
-export default TranslatorBtnBlock
+    setIsLoad(false);
+  }, []);
+
+  const changeLanguage = (languageUser) => {
+    localStorage.setItem("whatLanguage", languageUser);
+    const language = localStorage.getItem("whatLanguage");
+    setLanguage(language);
+    i18n.changeLanguage(language);
+  };
+  // return (<div className={className}>
+  //     {!isLoad && <LangSwitcher
+  //     changeLanguage={changeLanguage}
+  //     currentLanguage={language} />}
+  //     </div>)
+  return (
+    <>
+      {!isLoad && (
+        <LangSwitcher
+          changeLanguage={changeLanguage}
+          currentLanguage={language}
+          ref={ref}
+        />
+      )}
+    </>
+  );
+};
+
+export default TranslatorBtnBlock;
