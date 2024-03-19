f376790ebbece399a9435e729a7fc2c09ebf87c9
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-03-19 22:49:01 +0200

    fix

--- src/sections/blogIdSection/BlogIdSection.jsx
+++ src/sections/blogIdSection/BlogIdSection.jsx
@@ -14,6 +14,7 @@ import BreadCrumbs from '@/components/BreadCrumbs/BreadCrumbs';
 
 const BlogIdSection = ({ params }) => {
   const { slug } = params;
+
   const { data, error, isLoading } = GetIdDataFromSection('blogs', slug);
   const pathname = usePathname();
   const isPathExist = useCheckPathname(pathname);
