18f999d3b76f831c83795bfc4d989c42ab1d4b8f
Nataliia <natalabojko@gmail.com>
2024-04-10 17:20:07 +0300

    fixed setBurgermenu bugs

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,16 +1,18 @@
-"use client"
+"use client";
 import React from "react";
 import styles from "./Loading.module.scss";
 import { useTranslation } from "react-i18next";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+
 const Loading = ({ className }) => {
-  const{t}=useTranslation();
-  const[isLoad, setIsLoad]=useState(true)
-  useEffect(()=>setIsLoad(false),[])
-  return  <h3 className={`${styles.loading} ${className}`}>
-    {!isLoad && t('LoadStatus.Load')}
-    {/* Loading... */}
-    </h3>;
+  const { t } = useTranslation();
+  const [isLoad, setIsLoad] = useState(true);
+  useEffect(() => setIsLoad(false), []);
+  return (
+    <h3 className={`${styles.loading} ${className}`}>
+      {!isLoad && t("LoadStatus.Load")}
+    </h3>
+  );
 };
 
 export default Loading;
