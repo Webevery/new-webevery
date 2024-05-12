411050bc47fccaac0da8da7f1658f3541aa4c0a2
Maksimchik36 <Peltek1985@gmail.com>
2024-05-12 19:45:48 +0300

    fixed contactPage without fixing form

--- src/sections/contactsSection/OurContacts.jsx
+++ src/sections/contactsSection/OurContacts.jsx
@@ -1,3 +1,57 @@
+// import { useTranslation } from "react-i18next";
+// import { socialLinks } from "@/helpers/linkArrays";
+// import SocialLinksList from "@/components/SocialLinks/SocialLinksList";
+
+// import styles from "./ContactsSection.module.scss";
+
+// const OurContacts = () => {
+//     const { t } = useTranslation();
+//     return (
+//         <div className={styles.informSection}>
+//             <div className={styles.informBlock}>
+//                 <div>
+//                     <h2 className={`titleGradient ${styles.subTitle}`}>
+//                         {t("ContactsPage.Phone")}
+//                     </h2>
+//                     <a
+//                         className={`${styles.phoneText} navLinkHover`}
+//                         href='tel:+380966058605'
+//                     >
+//                         +380966058605
+//                     </a>
+//                 </div>
+//                 <div>
+//                     <h2 className={`titleGradient ${styles.subTitle}`}>
+//                         {t("ContactsPage.Email")}
+//                     </h2>
+//                     <a
+//                         className={`${styles.mailText} navLinkHover`}
+//                         href='mailto:inbox.webevery@gmail.com'
+//                     >
+//                         inbox.webevery@gmail.com
+//                     </a>
+//                 </div>
+//                 <div>
+//                     <h2 className={`titleGradient ${styles.subTitle}`}>
+//                         {t("ContactsPage.Social")}
+//                     </h2>
+//                     <SocialLinksList
+//                         list={socialLinks}
+//                         className={styles.socialList}
+//                     />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default OurContacts;
+
+
+
+
+
+
 import { useTranslation } from "react-i18next";
 import { socialLinks } from "@/helpers/linkArrays";
 import SocialLinksList from "@/components/SocialLinks/SocialLinksList";
@@ -7,41 +61,41 @@ import styles from "./ContactsSection.module.scss";
 const OurContacts = () => {
     const { t } = useTranslation();
     return (
-        <div className={styles.informSection}>
-            <div className={styles.informBlock}>
-                <div>
-                    <h2 className={`titleGradient ${styles.subTitle}`}>
-                        {t("ContactsPage.Phone")}
-                    </h2>
-                    <a
-                        className={`${styles.phoneText} navLinkHover`}
-                        href='tel:+380966058605'
-                    >
-                        +380966058605
-                    </a>
-                </div>
-                <div>
-                    <h2 className={`titleGradient ${styles.subTitle}`}>
-                        {t("ContactsPage.Email")}
-                    </h2>
-                    <a
-                        className={`${styles.mailText} navLinkHover`}
-                        href='mailto:inbox.webevery@gmail.com'
-                    >
-                        inbox.webevery@gmail.com
-                    </a>
-                </div>
-                <div>
-                    <h2 className={`titleGradient ${styles.subTitle}`}>
-                        {t("ContactsPage.Social")}
-                    </h2>
-                    <SocialLinksList
-                        list={socialLinks}
-                        className={styles.socialList}
-                    />
-                </div>
-            </div>
+        // <div className={styles.informSection}>
+        <div className={styles.informBlock}>
+            {/* <div> */}
+            <h2 className={`titleGradient ${styles.subTitle}`}>
+                {t("ContactsPage.Phone")}
+            </h2>
+            <a
+                className={`${styles.phoneText} navLinkHover`}
+                href='tel:+380966058605'
+            >
+                +380966058605
+            </a>
+            {/* </div> */}
+            {/* <div> */}
+            <h2 className={`titleGradient ${styles.subTitle}`}>
+                {t("ContactsPage.Email")}
+            </h2>
+            <a
+                className={`${styles.mailText} navLinkHover`}
+                href='mailto:inbox.webevery@gmail.com'
+            >
+                inbox.webevery@gmail.com
+            </a>
+            {/* </div> */}
+            {/* <div> */}
+            <h2 className={`titleGradient ${styles.subTitle}`}>
+                {t("ContactsPage.Social")}
+            </h2>
+            <SocialLinksList
+                list={socialLinks}
+                className={styles.contactsSocialList}
+            />
+            {/* </div> */}
         </div>
+        // </div>
     );
 };
 
