22c85fcb5627919f79e1d64f65601bb3bcb61291
volodymirkavun1 <volodymirkavun1@gmail.com>
2024-05-26 13:54:46 +0300

    add: OG_for_main_page

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,13 +27,59 @@ const noto_sans = Noto_Sans({
 });
 
 export const metadata = {
-  title: 'Webevery',
-  description: 'Webevery description',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_MAIN_URL),
+  title: "Розробка сайту — Webevery",
+  description:
+    "&#11088; Webevery &#11088; — Розробка сайту &#9989; Ваш надійний партнер у розробці веб-сайту &#9989; Створити сайт під ключ&#10071; Розробка програмного продукту&#9996;",
+  keywords: [
+    "Розробка сайту",
+    "Веб-сайти",
+    "Розробка програмного продукту",
+    "Створити сайт під ключ",
+    "Розробка сайту — Webevery",
+    "Webevery",
+    "Веб-студія Webevery",
+  ],
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_MAIN_URL,
+  },
+  openGraph: {
+    title: "Створення сайту — Webevery. Розробка програмного продукту.",
+    url: process.env.NEXT_PUBLIC_MAIN_URL,
+    description:
+      "Розробка сайту - Webevery &#11088; Розробка програмного продукту &#9996; Створити сайт під ключ&#10071;",
+    type: "website",
+    siteName: "Webevery",
+    images: [
+      {
+        url: "/seo_images/opengraph-image-400x300.png",
+        type: "image/png",
+        width: 400,
+        height: 300,
+        alt: "м",
+      },
+      {
+        url: "/seo_images/twitter-image-800x600.png",
+        type: "image/png",
+        width: 800,
+        height: 600,
+        alt: "Webevery",
+      },
+      {
+        url: "/seo_images/opengraph-image-1200-630.png",
+        type: "image/png",
+        width: 1200,
+        height: 630,
+        alt: "Webevery",
+      },
+    ],
+    locale: "uk-UA",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="uk-UA">
       <body className={`${exo.variable} ${noto_sans.variable}`}>
         <AuthProvider>
           <BackgroundAnimation />
