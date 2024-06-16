b4632cd588a5e76e5a98c681ff943d149d84c766
volodymirkavun1 <volodymirkavun1@gmail.com>
2024-06-16 19:25:05 +0300

    add: seo_blog_faq

--- src/app/(pages)/faq/page.jsx
+++ src/app/(pages)/faq/page.jsx
@@ -1,13 +1,26 @@
 import FAQ from "@/screens/faq/FAQ";
 
-export const metadata = {
-  title: "Розробка сайту — Webevery",
+export async function generateMetadata({ params, searchParams }, parent) {
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: "Розробка сайту — Webevery | FAQ &#128240;",
   description:
-    "Webevery &#11088; — Розробка сайту &#9989; Ваш надійний партнер у розробці веб-сайту &#9989; Розробка програмного продукту&#9996; Створити сайт під ключ&#10071;",
-  alternates: {
-    canonical: `${process.env.NEXT_PUBLIC_MAIN_URL}faq`,
-  },
-};
+    "Webevery &#9757; — Розробка сайту &#9989; Ваш надійний партнер у розробці веб-сайту &#9996; Замовити сайт&#128190; Створити сайт під ключ&#128273;",
+    openGraph: {
+      images: [parent?.images, ...previousImages],
+      type: "website",
+      title: "Створення сайту — Webevery&#127757;. Створення сайтів.",
+      url: `${process.env.NEXT_PUBLIC_MAIN_URL}faq`,
+      description:
+        "Створення сайту — Webevery&#127757;. Корисні статті&#128240;",
+    },
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_MAIN_URL}faq`,
+    },
+  };
+}
 
 const FAQPage = () => {
     const jsonLd = {
@@ -27,7 +40,7 @@ const FAQPage = () => {
         position: 2,
         item: {
           "@id": `${process.env.NEXT_PUBLIC_MAIN_URL}faq`,
-          name: "Створити сайт - Webevery &#11088; Розробка сайту &#9989;",
+          name: "Створити сайт - Webevery &#11088; Отримати готовий продукт",
         },
       },
     ],
